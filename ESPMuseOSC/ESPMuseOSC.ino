062ca65dcd0ec61ae22e99608866d49e97e9293a
ogokdemir <ozan.gokdemir15@ncf.edu>
2020-02-23 23:19:20 -0600

    Managed to send data from the MindMonitor to ESP8266 over wifi.

--- ESPMuseOSC/ESPMuseOSC.ino
+++ ESPMuseOSC/ESPMuseOSC.ino
@@ -1,37 +1,9 @@
-/*---------------------------------------------------------------------------------------------
+#include <ArduinoOSC.h>
 
-  ESP8266 Muse Monitor OSC Receiver Example by James Clutterbuck https://www.MuseMonitor.com
-
-  Requirements:
-  * Arduino OSC library - https://github.com/CNMAT/OSC
-  * ESP8266 NodeMCU 1.0 (ESP-12E Module)
-  * SSD1331 Color OLED (96x64)
-  
-  SSD1331 OLED Wiring:
-  ESP  -> GND, 3V3,  D5,  D7,  D4, D1, D2
-           |    |    |    |    |   |   |
-  OLED -> GND, VCC, SCL, SDA, RES, DC, CS
-
-  Features:
-  * Splashscreen showing Wifi SSID Name, IP Address and Port.
-  * Live OSC bundle message processing from Muse Monitor.
-  * Autodetection of Single or four channel absolute waves (/muse/elements/*_absolute).
-  * Horseshoe indicator showing headband fit (/muse/elements/horseshoe, /muse/elements/touching_forehead).
-  * Relative brainwaves (Delta, Theta, Alpha, Beta, Gamma) calculated from absolutes.
-  * Automatic timeout to splashscreen when no data received. 
- 
---------------------------------------------------------------------------------------------- */
 //Settings
-#define WIFI_SSID "*****"
-#define WIFI_PASSWORD "*****"
+#define WIFI_SSID "NETGEAR75"
+#define WIFI_PASSWORD "rusticgiant613"
 #define LISTEN_PORT 5000
-#define TIMEOUT_MILLIS 5000
-
-#define HSI_X 85
-#define HSI_Y 9
-#define WAVE_SPACING 18
-#define LINE_HEIGHT_MAX 50
-#define OLED_HEIGHT 63
 
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
@@ -39,20 +11,9 @@
 #include <OSCBundle.h>
 #include <OSCData.h>
 
-//SSD1331 Color OLED driver. 96x64
-#include <SPI.h>
-#include <SSD_13XX.h>
-#include "_fonts/unborn_small.c"
 
-//OLED Wiring Pins
-#define __CS1    D2
-#define __DC     D1
-#define LCDRESET D4
-
-SSD_13XX display = SSD_13XX(__CS1, __DC);
 WiFiUDP Udp;
 
-bool splashScreenCleared = false;
 
 //Absolute
 float da = 0;
@@ -68,84 +29,20 @@ float ar = 0;
 float br = 0;
 float gr = 0;
 
-//Height
-int drh = 0;
-int trh = 0;
-int arh = 0;
-int brh = 0;
-int grh = 0;
 
-int hsiTF = 0;//Touching Forehead
-
-long timeoutTargetMS = 0;
-
-#include "display_funcs.h"
 
 void setup() {
   //Reset LCD in case intermittent power turned it off.
-  pinMode(LCDRESET,OUTPUT);
-  digitalWrite(LCDRESET,LOW);
-  delay(500);
-  digitalWrite(LCDRESET,HIGH);
-  delay(100);
-  
-  display.begin();
-  display.setTextScale(1);
-  display.fillScreen(BLACK);
-  
-  Serial.begin(115200);
-  Serial.println("\n\nESP Booted.");
-  
+  //pinMode(LCDRESET,OUTPUT);
+  //digitalWrite(LCDRESET,LOW);
+  //delay(500);
+  //digitalWrite(LCDRESET,HIGH);
+  //delay(100);
+
   setupWifi();
-  showSplashScreen();
 }
 
-void touchingForehead(OSCMessage &msg) {
-  hsiTF = msg.getInt(0);
-}
 
-void horseshoe(OSCMessage &msg) {
-  int hsiTP9  = (int)msg.getFloat(0);
-  int hsiAF7  = (int)msg.getFloat(1);
-  int hsiAF8  = (int)msg.getFloat(2);
-  int hsiTP10 = (int)msg.getFloat(3);
-  
-  //Touching Forehead
-  hsiTF?display.fillCircle(HSI_X, HSI_Y-6, 2, BLACK):drawRing(HSI_X, HSI_Y-6);
-
-  //Draw HSI 1=Good, 2=Medium, 4=Bad
-  if (hsiTP9==1){
-    display.fillCircle(HSI_X-6, HSI_Y+3, 2, GREEN);
-  } else if (hsiTP9==2){
-    drawRing(HSI_X-6, HSI_Y+3);
-  } else {
-    display.fillCircle(HSI_X-6, HSI_Y+3, 2, WHITE);
-  }  
-
-  if (hsiAF7==1){
-    display.fillCircle(HSI_X-6, HSI_Y-3, 2, GREEN);
-  } else if (hsiAF7==2){
-    drawRing(HSI_X-6, HSI_Y-3);
-  } else {
-    display.fillCircle(HSI_X-6, HSI_Y-3, 2, WHITE);
-  }
-
-  if (hsiAF8==1){
-    display.fillCircle(HSI_X+6, HSI_Y-3, 2, GREEN);
-  } else if (hsiAF8==2){
-    drawRing(HSI_X+6, HSI_Y-3);
-  } else {
-    display.fillCircle(HSI_X+6, HSI_Y-3, 2, WHITE);
-  }
-
-  if (hsiTP10==1){
-    display.fillCircle(HSI_X+6, HSI_Y+3, 2, GREEN);
-  } else if (hsiTP10==2){
-    drawRing(HSI_X+6, HSI_Y+3);
-  } else {
-    display.fillCircle(HSI_X+6, HSI_Y+3, 2, WHITE);
-  }
-}
 
 float getAveragePSD(OSCMessage &msg) {
   if (msg.size()==1){
@@ -160,31 +57,26 @@ void delta(OSCMessage &msg) {
   //Calculate relative value
   dr = (pow(10,da) / (pow(10,da) + pow(10,ta) + pow(10,aa) + pow(10,ba) + pow(10,ga)));
   //map to screen height
-  drh = (int)mapfloat(dr, 0.0, 1.0, 0.0, LINE_HEIGHT_MAX);
 }
 
 void theta(OSCMessage &msg) {
   ta = getAveragePSD(msg);
   tr = (pow(10,ta) / (pow(10,da) + pow(10,ta) + pow(10,aa) + pow(10,ba) + pow(10,ga)));
-  trh = (int)mapfloat(tr, 0.0, 1.0, 0.0, LINE_HEIGHT_MAX);
 }
 
 void alpha(OSCMessage &msg) {
   aa = getAveragePSD(msg);
   ar = (pow(10,aa) / (pow(10,da) + pow(10,ta) + pow(10,aa) + pow(10,ba) + pow(10,ga)));
-  arh = (int)mapfloat(ar, 0.0, 1.0, 0.0, LINE_HEIGHT_MAX);
 }
 
 void beta(OSCMessage &msg) {
   ba = getAveragePSD(msg);
   br = (pow(10,ba) / (pow(10,da) + pow(10,ta) + pow(10,aa) + pow(10,ba) + pow(10,ga)));
-  brh = (int)mapfloat(br, 0.0, 1.0, 0.0, LINE_HEIGHT_MAX);
 }
 
 void gamma(OSCMessage &msg) {
   ga = getAveragePSD(msg);
   gr = (pow(10,ga) / (pow(10,da) + pow(10,ta) + pow(10,aa) + pow(10,ba) + pow(10,ga)));
-  grh = (int)mapfloat(gr, 0.0, 1.0, 0.0, LINE_HEIGHT_MAX);
 }
 
 void loop() {
@@ -197,36 +89,53 @@ void loop() {
     }
     
     if (!bundle.hasError()) {
-      if (splashScreenCleared==false){
-        clearSplashScreen();
-      }
-      bundle.dispatch("/muse/elements/horseshoe", horseshoe);
-      bundle.dispatch("/muse/elements/touching_forehead", touchingForehead);
       bundle.dispatch("/muse/elements/delta_absolute", delta);
       bundle.dispatch("/muse/elements/theta_absolute", theta);
       bundle.dispatch("/muse/elements/alpha_absolute", alpha);
       bundle.dispatch("/muse/elements/beta_absolute", beta);
       bundle.dispatch("/muse/elements/gamma_absolute", gamma);
-    }
-    //Reset timeout
-    timeoutTargetMS=millis()+TIMEOUT_MILLIS;
-  } else {    
-    //Check for timeout
-    if(splashScreenCleared && millis()>timeoutTargetMS){
-      splashScreenCleared=false;
-      showSplashScreen();
-    }
-  }
 
-  if (splashScreenCleared){
-    //Relative waves
-    renderVBar(0, drh, RED);
-    renderVBar(WAVE_SPACING, trh, PURPLE);
-    renderVBar(WAVE_SPACING*2, arh, BLUE);
-    renderVBar(WAVE_SPACING*3, brh, GREEN);
-    renderVBar(WAVE_SPACING*4, grh, ORANGE);
-  }
+      //Serial.println(br);
+      Serial.println(ar);
+      //Serial.println(tr);
+      //Serial.println(dr);
+      //Serial.println(gr);
+
+    }
+  } 
 }
 
 
 
+//Configures and initiates the UDP packet flow from the MindMonitor app to the ESP8266 chip.
+//WiFi.config(static ip for the esp8266, gateway's local ip, subnet)
+void setupWifi(){
+  Serial.begin(115200);
+  Serial.println("\n\nESP Booted.");
+  
+  WiFi.config(IPAddress(192,168,0,123),IPAddress(192,168,0,2), IPAddress(255,255,255,0)); 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("Starting UDP");
+    Udp.begin(LISTEN_PORT);
+    Serial.print("Local port: ");
+    Serial.println(Udp.localPort());
+    
+}
+
+
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+ return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
